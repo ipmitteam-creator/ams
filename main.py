6344c43070b17e393c88f54db2f6b41bbbce7e92
Rishi Madan <rishimadan@Rishis-MacBook-Air.local>
2025-09-11 19:29:06 +0530

    added report python in main

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# main.py
 from fastapi import FastAPI
 from app.crud.sewadar import router as sewadar_router
 from app.crud.department import router as department_router
-from app.crud.attendance import router as attendance_router   # 👈 NEW
+from app.crud.attendance import router as attendance_router
+from app.crud.report import router as report_router   # 👈 NEW
 
 app = FastAPI(
     title="AMS API",
@@ -13,10 +13,10 @@ app = FastAPI(
 # ---------------- Include Routers ----------------
 app.include_router(sewadar_router, prefix="/sewadar", tags=["Sewadar"])
 app.include_router(department_router, prefix="/department", tags=["Department"])
-app.include_router(attendance_router, prefix="/attendance", tags=["Attendance"])  # 👈 NEW
+app.include_router(attendance_router, prefix="/attendance", tags=["Attendance"])
+app.include_router(report_router, prefix="/attendance", tags=["Reports"])  # 👈 NEW
 
 # ---------------- Root Endpoint ----------------
 @app.get("/")
 def root():
     return {"message": "Welcome to AMS API. Visit /docs for API documentation."}
-
